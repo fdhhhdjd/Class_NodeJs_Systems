1bbdd1f32957720233eada9980634a1288979da6
fdhhhdjd <nguyentientai10@gmail.com>
2024-01-21 10:50:50 +0700

    feat: add connect knex

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@
 const express = require("express");
 
 const app = express();
+
+require("dotenv").config();
+
 app.use(express.json());
+
+require("./databases/init.postgresql");
 
 app.use("/api", require("./routes/todo"));
 
