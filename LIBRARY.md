5c9ea0c65531fbaf3da8dd9abe58dc96e8649524
fdhhhdjd <nguyentientai10@gmail.com>
2024-01-23 15:12:19 +0700

    feat: add ioredis

--- LIBRARY.md
+++ LIBRARY.md
@@ -1,3 +1,5 @@
+## [IoRedis example](https://github.com/redis/ioredis/blob/HEAD/examples)
+
 # 1. Check helmet
 ```cmd
     curl -I http://localhost:5000
--- src/app/v1/routes/index.js
+++ src/app/v1/routes/index.js
@@ -10,6 +10,7 @@ const {
 } = require("../../../commons/utils/httpStatusCode");
 const redisInstance = require("../../../databases/init.redis");
 const { Test } = require("../../../commons/keys/test");
+const { User } = require("../../../commons/keys/user");
 
 const router = express.Router();
 
@@ -18,10 +19,10 @@ router.use("/v1/todos", require("./todos"));
 router.use("/v1/labels", require("./labels"));
 
 router.get("/v1", async (_, res, __) => {
-  const testRedis = await redisInstance.get(Test);
+  const resultProfileUser = await redisInstance.hgetall(User);
   const healthCheck = {
     uptime: process.uptime(),
-    message: testRedis || ReasonPhrases.OK,
+    message: resultProfileUser || ReasonPhrases.OK,
     timestamp: Date.now(),
   };
   return res.status(StatusCodes.OK).json(healthCheck);
