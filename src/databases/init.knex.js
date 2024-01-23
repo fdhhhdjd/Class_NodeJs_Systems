184d9c7302ce8c5fadce3c46afb765aa186439ee
fdhhhdjd <nguyentientai10@gmail.com>
2024-01-23 17:08:21 +0700

    fix: delete comment

--- src/databases/init.knex.js
+++ src/databases/init.knex.js
@@ -6,12 +6,12 @@ const knexInstance = KNEX({
   client: "cockroachdb",
   connection: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?${process.env.SECURITY}`,
   pool: {
-    min: 10, // Số kết nối tối thiểu được giữ trong pool.
-    max: 20, // Số kết nối tối đa mà pool có thể giữ.
-    acquireTimeoutMillis: 30000, // Thời gian tối đa  mà một kết nối mới có thể chờ đợi để được tạo và thêm vào pool.
-    createTimeoutMillis: 30000, // Thời gian tối đa mà một kết nối mới có thể mất để được tạo.
-    idleTimeoutMillis: 30000, // Thời gian tối đa mà một kết nối có thể ở trong pool mà không được sử dụng trước khi bị giải phóng.
-    reapIntervalMillis: 1000, //  Thời gian giữa các chu kỳ để giải phóng các kết nối không sử dụng.
+    min: 10,
+    max: 20,
+    acquireTimeoutMillis: 30000,
+    createTimeoutMillis: 30000,
+    idleTimeoutMillis: 30000,
+    reapIntervalMillis: 1000,
   },
   debug: process.env.NODE_ENV === NODE_ENV.DEV,
 });
