5d0d4f45b51208da76a3511cc303d58369ea8bfe
fdhhhdjd <nguyentientai10@gmail.com>
2024-04-19 13:18:51 +0700

    feat: add demo hash password

--- demo.js
+++ demo.js
@@ -1,9 +1,33 @@
-const yourself = new Map();
+const bcrypt = require("bcrypt");
 
-yourself
-  .set("fullname", "Nguyen Tien Tai")
-  .set("age", 24)
-  .set("job", "developer and teacher");
+const saltRounds = 10;
 
-const valueForKeyAge = yourself.get("fullname");
-console.log("Value for age:", valueForKeyAge);
+// const handlePassword = async (password) => {
+//   const salt = "$2b$10$QEbg.e6ig1I4rSu4ljIxDu";
+//   // const salt = await bcrypt.genSalt(saltRounds);
+
+//   console.log(`salt:: ${salt}`);
+
+//   const hash = await bcrypt.hash(password, salt);
+//   console.log(`Hash:: ${hash}`);
+// };
+
+const handlePassword = async (password) => {
+  const saltRounds = 10;
+  // const salt = await bcrypt.genSalt(saltRounds);
+  const salt = "$2b$10$U0Lp2zaHtzqC/.61cGCtZe";
+
+  //  $2b$10$gqjRx4X/yTzZS5MIzVvlAu  tram1234  $2b$10$gqjRx4X/yTzZS5MIzVvlAuqoMhmYdv5ffWWY2yxTO/uWrTxtA0VXy
+  // $2b$10$.n7NELKxdFWpefx6TKBqIO nam1234   $2b$10$.n7NELKxdFWpefx6TKBqIO.InBI.YBPO5..mbbMs6SppvqX0UnDf.
+  // $2b$10$U0Lp2zaHtzqC/.61cGCtZe phong  $2b$10$U0Lp2zaHtzqC/.61cGCtZeEuB.gyxphozW37NxmfUUNTyeNGmP81q
+
+  console.log(`salt:: ${salt}`);
+
+  const hash = await bcrypt.hash(password, salt);
+
+  console.log(`Hash:: ${hash}`);
+};
+
+handlePassword("phong");
+
+// $2b$10$0Ihy6yvhvb3ZMOgaaIBH6e
